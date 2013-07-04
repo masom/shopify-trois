5e9b8ad1ace2b253cdb9791dec61e4beaac4e132
Martin Samson <pyrolian@gmail.com>
2013-07-04 15:18:15 -0400

    Json engine class documentation.

--- shopify_trois/engines/http/json.py
+++ shopify_trois/engines/http/json.py
@@ -17,14 +17,33 @@ class Json(OAuthEngine):
     extension = 'json'
     mime = 'application/json; charset=utf-8'
 
-    def __init__(self, shop_name, credentials, ignore_supported=False):
+    def __init__(self, shop_name, credentials, ignore_supported = False,
+            ignore_model_properties = False):
+        """Initialize the Json engine.
+
+        :param shop_name: The name of the shopify store.
+        :param credentials: :class:`~shopify_trois.credential.Credential`
+        :param ignore_supported: When set to True, the engine will ignore
+                                 the ``supported`` property of the models.
+        :param ignore_model_properties: When set to True, the engine will
+                                        ignore the ``properties`` property of
+                                        models when persisting them.
+        """
+
         super().__init__(shop_name = shop_name, credentials = credentials)
 
-        """When set to True, ignore checking for supported actions on models."""
+        #: When set to True, ignore checking for supported actions on models.
         self.ignore_supported = ignore_supported
 
-    def add(self, instance, fields):
+    def add(self, instance):
+        """Add the model instance to the store.
+
+        :param instance: The model instance being added.
+        """
+
         self._can_request("create", instance)
+
+        #TODO use the model `properties` when generating the json document.
 
         req = Request(instance)
         req.data = json.dumps(instance.to_dict())
@@ -37,10 +56,21 @@ class Json(OAuthEngine):
         raise ShopifyException(res)
 
     def remove(self, instance):
+        """Remove (delete) a model instance.
+
+        An InvalidRequestException will be raised if the instance does not yet
+        exists.
+
+        :param instance: The model instance to remove.
+        """
+
         self._can_request("delete", instance)
 
-        req = Request(instance)
+        if not instance._meta.exists:
+            msg = "The supplied instance does not yet exists."
+            raise InvalidRequestException(msg)
 
+        req = Request(instance)
         res = self.delete(req)
 
         if res.status_code == requests.codes.ok:
@@ -48,8 +78,24 @@ class Json(OAuthEngine):
 
         raise ShopifyException(res)
 
-    def update(self, instance, fields):
+    def update(self, instance, whitelist = None):
+        """Update a model instance.
+
+        An InvalidRequestException will be raised if the instance has not been
+        marked as existing.
+
+        :param instance: The model instance being updated.
+        :param whitelist: A list of attributes to be updated. If set to None,
+                          all modified attributes will be updated.
+        """
+
         self._can_request("update", instance)
+
+        if not instance._meta.exists:
+            msg = "The supplied instance does not yet exists."
+            raise InvalidRequestException(msg)
+
+        #TODO use the model `properties` when generating the json document.
 
         req = Request(instance)
         req.data = json.dumps(instance.to_dict())
@@ -61,7 +107,13 @@ class Json(OAuthEngine):
 
         raise ShopifyException(res)
 
-    def view(self, Model, primary_key):
+    def view(self, model, primary_key):
+        """Get a specific model instance by primary key.
+
+        :param Model: The class being queried.
+        :param primary_key: The primary key value of the instance.
+        """
+
         self._can_request("view", instance)
 
         req = Request(model)
@@ -75,6 +127,12 @@ class Json(OAuthEngine):
         raise ShopifyException(res)
 
     def index(self, model, **params):
+        """Fetch the index for a given model and supplied parameters.
+
+        :param model: The model being queried.
+        :param options: Query parameters (see shopify documentation)
+        """
+
         self._can_request("index", instance)
 
         req = Request(model)
@@ -88,14 +146,25 @@ class Json(OAuthEngine):
         raise ShopifyException(res)
 
     def authorize_app_url(self):
+        """Generates the oauth authorization url."""
+
         return self.oauth_authorize_url()
 
     def setup_access_token(self):
+        """Utility function wrapping getting the oauth token and setting
+        the credential values.
+        """
+
         oauth_token = self.oauth_access_token()
         self.credentials.code = None
         self.credentials.oauth_access_token = data['access_token']
 
     def oauth_access_token(self):
+        """Fetch the OAuth access token from shopify.
+
+        :meth:`setup_access_token` should be used.
+        """
+
         url = self.oauth_access_token_url()
         headers = { "Content-Type": self.mime }
 
@@ -104,17 +173,30 @@ class Json(OAuthEngine):
         if r.status_code == requests.codes.ok:
             data = r.json()
             return data['access_token']
-        else:
-            raise ShopifyException(r.text())
+
+        raise ShopifyException(r.text())
 
     def _can_request(self, method, model):
-        if not method in model.supported and not self.ignore_supported:
-            msg = "`%s` is not supported by %s."
+        """Verify the request method is supported by the provided model.
 
-            if isinstance(model, type):
-                model_name = model.__name__
-            else:
-                model_name = model.__class__.__name__
+        If the request method is not supported, an InvalidRequestException
+        will be raised.
 
-            raise InvalidRequestException(msg % (method, model_name))
+        :param method: The method to be executed (index, create, delete, etc.)
+        :param model: A class or instance the request will be made for.
+        """
+
+        if method in model.supported or self.ignore_supported:
+            #The method is either supported or the engine completely ignores
+            #the check
+            return
+
+        msg = "`%s` is not supported by %s."
+
+        if isinstance(model, type):
+            model_name = model.__name__
+        else:
+            model_name = model.__class__.__name__
+
+        raise InvalidRequestException(msg % (method, model_name))
 
