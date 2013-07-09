8e8381c09249b8ee323859ec3f33c0ec6e91583c
Martin Samson <pyrolian@gmail.com>
2013-07-08 21:47:35 -0400

    Removed uneeded build.

--- build/lib/shopify_trois/__init__.py
+++
@@ -1,5 +0,0 @@
-__version__ = '0.4-dev'
-
-from .collection import Collection
-from .credentials import Credentials
-from .exceptions import *
--- build/lib/shopify_trois/exceptions.py
+++
@@ -1,6 +0,0 @@
-class ShopifyException(Exception):
-    pass
-
-
-class InvalidRequestException(ShopifyException):
-    pass
