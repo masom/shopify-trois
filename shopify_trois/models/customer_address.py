7f9b40d950cfdf9e6a8194319bf8036de927859d
Martin Samson <martin@doorbot.co>
2015-08-31 17:53:24 -0400

    CustomerAddress

---
+++ shopify_trois/models/customer_address.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+    shopify_trois.models.customer_address
+
+    Shopify-Trois CustomerAddress
+
+    :copyright: (c) 2015 by Martin Samson
+    :license: MIT, see LICENSE for more details.
+"""
+
+from .model import Model
+
+
+class CustomerAddress(Model):
+    """CustomerAddress
+    """
+
+    resource = ""
+
+    supported = [
+    ]
+
+    properties = [
+    ]
